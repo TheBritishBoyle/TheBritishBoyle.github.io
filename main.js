4b359173d716086a2dbe01733af1ceb0da08653c
TheBritishBoyle <49663536+TheBritishBoyle@users.noreply.github.com>
2023-03-07 16:44:39 -0500

    Update main.js

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ buttons.forEach((button) => {
 
 
 const texts = document.querySelectorAll(".job");
-const imgButtons = document.querySelectorAll(".job-image");
+const imgButtons = document.querySelectorAll(".job-column");
 
 imgButtons.forEach((imgButton, butIndex) => {
   imgButton.addEventListener("click", () => {
@@ -62,3 +62,63 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.scroll-hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+/* test */
+
+const wrapper = document.getElementById("tiles");
+
+let columns = 0,
+    rows = 0,
+    toggled = false;
+
+const toggle = () => {
+  toggled = !toggled;
+  
+  document.body.classList.toggle("toggled");
+}
+
+const handleOnClick = index => {
+  toggle();
+  
+  anime({
+    targets: ".tile",
+    opacity: toggled ? 0 : 1,
+    delay: anime.stagger(50, {
+      grid: [columns, rows],
+      from: index
+    })
+  });
+}
+
+const createTile = index => {
+  const tile = document.createElement("div");
+  
+  tile.classList.add("tile");
+  
+  tile.style.opacity = toggled ? 0 : 1;
+  
+  tile.onclick = e => handleOnClick(index);
+  
+  return tile;
+}
+
+const createTiles = quantity => {
+  Array.from(Array(quantity)).map((tile, index) => {
+    wrapper.appendChild(createTile(index));
+  });
+}
+
+const createGrid = () => {
+  wrapper.innerHTML = "";
+  
+  const size = document.body.clientWidth > 800 ? 100 : 50;
+  
+  columns = Math.floor(document.body.clientWidth / size);
+  rows = Math.floor(document.body.clientHeight / size);
+  
+  createTiles(columns * rows);
+}
+
+createGrid();
+
+window.onresize = () => createGrid();
